cba416e167001dfca8ce842855f9ae82f31c944a
kaustubh-s1 <shuklakaustubh84@gmail.com>
2023-04-13 23:53:07 +0530

    ✌🏻 add main, fine-tuning options: wrapping up

---
+++ marvin/__main__.py
@@ -0,0 +1,46 @@
+import sys
+from .user import User
+from . import gpt, config, logs
+
+def main():
+  if(len(sys.argv) > 1 and sys.argv[1] == '--add-openai-key'):
+    add_openai_key(sys.argv)
+  else:
+    run(sys.argv)
+
+def run(argv):
+  logs.debug('[Debug mode]')
+
+  user = User(argv)
+  status = user.init()
+  if(status == True):
+    user_input = user.get_input()
+
+    bash_result = gpt.generate_bash(user_input)
+
+    if(bash_result):
+      print('\n$ ' + bash_result + '\n')
+      feedback = user.set_feedback()
+
+      if (feedback == 'Run command'):
+        user.run_command(bash_result)
+      elif (feedback == 'Tweak the command'):
+        user.run_command(user.tweak_the_command(bash_result))
+      elif (feedback == 'Quit'):
+        pass
+    else:
+      print('\nUsage: marvin --add-openai-key <key>\n')
+  elif(status == False):
+    logs.error['arg']('Please provide an input, ex: python marvin.py turn off the bluetooth')
+  else:
+    logs.error['config'](status)
+
+def add_openai_key(argv):
+  if len(argv) <= 2:
+    logs.error['arg']('No key provided.')
+  else:
+    config.set('OPENAI_API_KEY', argv[2])
+    print('OpenAI API Key added.')
+
+if __name__ == '__main__':
+  main()
---
+++ marvin/fine_tune_data.py
@@ -0,0 +1,61 @@
+from tempfile import NamedTemporaryFile
+import csv, shutil
+from . import config, logs
+
+HISTORY_FILE_PATH = config.get('HISTORY_FILE_NAME')
+
+class FineTuneData:
+  def __init__(self, file_path_name=HISTORY_FILE_PATH):
+    self.file_path_name = file_path_name
+    try:
+      f = open(file_path_name, 'x')
+      logs.debug(file_path_name + ' file created.')
+      f.close()
+    except:
+      logs.debug(file_path_name + ' file found.')
+    finally:
+      self.file = open(file_path_name, 'r+')
+  
+  def __del__(self):
+    self.file.close()
+
+  def has_wrong_format(self, file):
+    reader = csv.reader(file, delimiter=',')
+    for row in reader:
+      if(len(row) != 2):
+        return True
+    return False
+  
+  def error(self):
+    if(self.has_wrong_format(self.file)):
+      return self.file_path_name + ' is in a wrong format, remove this file to solve the problem.'
+    else:
+      return False
+
+  def save(self, txt_input, txt_output):
+    logs.debug(self.__class__.__name__ + '.save("' + txt_input + '", "' + txt_output + '"):')
+    tempfile = NamedTemporaryFile('w+t', delete=False)
+
+    with open(self.file_path_name, 'r') as csvFile, tempfile:
+      edited = False
+      reader = csv.reader(csvFile, delimiter=',')
+      writer = csv.writer(tempfile, delimiter=',')
+
+      for row in reader:
+        if(row[0] == txt_input):
+          row[1] = txt_output
+          edited = True
+        writer.writerow(row)
+      
+      if not edited:
+        writer.writerow([txt_input, txt_output])
+      
+    shutil.move(tempfile.name, self.file_path_name)
+  
+  def get_ques_ans(self):
+    output = ""
+    self.file.seek(0)
+    reader = csv.reader(self.file, delimiter=',')
+    for row in reader:
+      output += "\nQ: " + row[0] + "\nA: " + row[1]
+    return output
