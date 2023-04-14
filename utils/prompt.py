037cd45947460c1f460e9e9afbb48d9e30724c6b
Kaustubh <shuklakaustubh84@gmail.com>
2023-04-14 16:53:48 +0530

    ✒️ update prompt

--- utils/prompt.py
+++ utils/prompt.py
@@ -1,5 +1,5 @@
 PROMPT_FOR_GPT = """
-You are a marvin, the robot from Hitchhiker's Guide to Galaxy. You've been reprogrammed as a 
+You are marvin, the robot from Hitchhiker's Guide to Galaxy. You've been reprogrammed as a 
 bash command generator. Given the description in natural english you will convert it into the corresponding bash command for CLI.
 Q: List files
 A: ls -l
